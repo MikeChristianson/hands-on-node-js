4a5973fb368c4436d54a943d34b0bbae1c1af64c
Mike Christianson <mike.christianson@meltmedia.com>
2014-06-11 13:25:17 -0700

    ex3

--- ex1.js
+++ ex1.js
@@ -1,14 +1,13 @@
 var b = new Buffer(100);
-
 for(var i = 0; i < b.length; i++) {
   b[i] = i;
 }
-
 console.log(b);
 
-var bs = new Buffer(20);
-
-b.copy(bs, 0, 40, 60);
-
+var bs = b.slice(40, 60);
 console.log(bs);
+
+var bcopy = new Buffer(20);
+b.copy(bcopy, 0, 40, 60);
+console.log(bcopy);
 
