2e1a719e2f6960ee3f24228d3a1a98b60a3a98c8
Mike Christianson <mike@codeaweso.me>
2014-06-17 19:37:25 -0700

    ex2

--- fs/ex1.js
+++ fs/ex1.js
@@ -7,7 +7,16 @@ var Promise = require('bluebird');
 Promise.promisifyAll(fs);
 
 fs.statAsync('../a.txt').then(function (stats) {
-  console.log(stats.size);
+  console.log('The file is ' + stats.size + ' bytes.');
+}).catch(function(e){
+  console.error("unable to read file", e);
+});
+
+fs.openAsync('../a.txt', 'r').then(function (fd) {
+  var buffer = new Buffer(5);
+  return fs.readAsync(fd, buffer, 0, buffer.length, 10);
+}).then(function (result) {
+  console.log('The buffer is "' + result[1].toString() + '".');
 }).catch(function(e){
   console.error("unable to read file", e);
 });
