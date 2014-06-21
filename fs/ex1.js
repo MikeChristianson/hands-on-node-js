bb00db897e0d7319f21da4d2720a4e36facfcb53
Mike Christianson <mike@codeaweso.me>
2014-06-21 09:47:33 -0700

    ex3

--- fs/ex1.js
+++ fs/ex1.js
@@ -14,22 +14,14 @@ fs.statAsync('../a.txt').then(function (stats) {
 
 fs.openAsync('../a.txt', 'r').then(function (fd) {
   var buffer = new Buffer(5);
-  return fs.readAsync(fd, buffer, 0, buffer.length, 10);
-}).then(function (result) {
-  console.log('The buffer is "' + result[1].toString() + '".');
-}).catch(function(e){
-  console.error("unable to read file", e);
-});
-
-fs.openAsync('../a.txt', 'r').then(function (fd) {
-  var buffer = new Buffer(5);
   return fs.readAsync(fd, buffer, 0, buffer.length, 5).then(function (result) {
     console.log('The buffer is "' + result[1].toString() + '".');
-    buffer = new Buffer(5);
     return fs.readAsync(fd, buffer, 0, buffer.length, 10).then(function (result) {
       console.log('The buffer is "' + result[1].toString() + '".');
     });
   }).catch(function (e) {
     console.error("unable to read file", e);
+  }).finally(function () {
+    fs.close(fd);
   });
 });
