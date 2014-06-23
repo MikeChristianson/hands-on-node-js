11b0c1e670e812e95f6ebb176ddac9733770eba2
Mike Christianson <mike@codeaweso.me>
2014-06-22 23:18:51 -0700

    refactored into top-down, pyramid-style

--- tcp/chitchat.js
+++ tcp/chitchat.js
@@ -8,29 +8,30 @@ function handleNewConnection(socket) {
   console.log('got new connection');
   chatters.push(socket);
 
-  function relayDataToOtherChatters(sourceSocket, data) {
-    chatters
-      .filter(function (socket) { return socket !== sourceSocket })
-      .forEach(function (socket) { socket.write(data) });
-  }
-
-  function handleIncomingData(data) {
-    console.log('someone said', data.toString());
-    relayDataToOtherChatters(socket, data);
-  }
-
-  function handleConnectionClosed() {
-    console.log('someone said goodbye');
-    _.remove(chatters, function (s) { return socket === s });
-  }
-
-  function handleError(error) {
-    console.warn('someone had a problem', error);
-  }
-
-  socket.on('data', handleIncomingData);
-  socket.on('end', handleConnectionClosed);
+  socket.on('data', _.partial(handleIncomingData, socket));
+  socket.on('end', _.partial(handleConnectionClosed, socket));
   socket.on('error', handleError);
 };
 
 net.createServer(handleNewConnection).listen(3000);
+
+function relayDataToOtherChatters(sourceSocket, data) {
+  chatters
+    .filter(function (socket) { return socket !== sourceSocket })
+    .forEach(function (socket) { socket.write(data) });
+}
+
+function handleIncomingData(socket, data) {
+  console.log('someone said', data.toString());
+  relayDataToOtherChatters(socket, data);
+}
+
+function handleConnectionClosed(socket) {
+  console.log('someone said goodbye');
+  _.remove(chatters, function (s) { return socket === s });
+}
+
+function handleError(error) {
+  console.warn('someone had a problem', error);
+}
+
